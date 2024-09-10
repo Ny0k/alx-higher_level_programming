eec1018bc3dd48d061b943a14cfbe7654c54d6ca
Ny0k <nyokakey@gmail.com>
2024-09-10 07:49:48 -0700

    0-run

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,16 +2,16 @@
 #define LISTS_H
 
 #include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
+
 typedef struct listint_s
+
 {
 	int n;
 	struct listint_s *next;
@@ -22,4 +22,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
